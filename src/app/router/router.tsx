a17fd1d5b528a108f65f4b3dafdeb5f6ba299f2c
Mariano <marianoarrigoni@outlook.com>
2026-02-25 21:52:59 +0100

    feat: add product details route

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,5 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import { ProductDetailsPage } from "@pages/product-details";
 import { ProductsPage } from "@pages/products";
 
-export const router = createBrowserRouter([{ path: "/", element: <ProductsPage /> }]);
+export const router = createBrowserRouter([
+  { path: "/", element: <ProductsPage /> },
+  { path: "/products/:id", element: <ProductDetailsPage /> },
+]);
